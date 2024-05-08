ec1b34ce59bff7aa3254c33b6c513da0635d686f
Pascal Greilach <pascal@greilach.dev>
2024-05-08 16:29:46 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Autostart database
+
+Configuration to start a docker container with PostgreSQL every time I load a VS Code workspace. [See the corresponding blog post](https://greilach.dev/blog/post/local-development-with-postgres) to read up on what exactly it does.
---
+++ database/run_postgres.sh
@@ -0,0 +1,26 @@
+HOST_PORT=5432
+NAME=postgres-dev
+DOCKER_REPO=postgres
+TAG=15.4
+
+cleanup() {
+    echo "Stopping docker container..."
+    docker stop ${NAME}
+}
+trap 'cleanup' SIGTERM EXIT
+
+docker run --rm --name $NAME \
+  --volume `pwd`/pgdata:/var/lib/pgsql/data \
+  --volume `pwd`/mnt_data:/mnt/data \
+  --volume `pwd`/pg_hba.conf:/etc/postgresql/pg_hba.conf \
+  --volume `pwd`/postgresql.conf:/etc/postgresql/postgresql.conf \
+  -e POSTGRES_PASSWORD=password \
+  -e POSTGRES_USER=postgres \
+  -e PGDATA=/var/lib/pgsql/data/pgdata14 \
+  -e POSTGRES_INITDB_ARGS="--data-checksums --encoding=UTF8" \
+  -e POSTGRES_DB=db \
+  -p ${HOST_PORT}:5432 \
+  ${DOCKER_REPO}:${TAG} \
+  postgres \
+    -c 'config_file=/etc/postgresql/postgresql.conf' \
+    -c 'hba_file=/etc/postgresql/pg_hba.conf'
